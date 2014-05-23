59020e2e4eb56d7ead35c5ef35a201c41cd69191
NCrashed <NCrashed@gmail.com>
2014-05-24 02:06:34 +0400

    Additional header to satisfy GPL recommendations.

--- source/util/vector.d
+++ source/util/vector.d
@@ -1,4 +1,20 @@
 ﻿// written in the D programming language
+/*
+*   This file is part of FostomEngine.
+*   
+*   FostomEngine is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*   
+*   FostomEngine is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*   
+*   You should have received a copy of the GNU General Public License
+*   along with FostomEngine.  If not, see <http://www.gnu.org/licenses/>.
+*/
 /**
 *   Copyright: © 2012-2014 Anton Gushcha
 *   License: Subject to the terms of the GPL-3.0 license, as written in the included LICENSE file.
