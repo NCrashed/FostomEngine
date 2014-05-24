9130c4da2e0ca9c0d7ff7ef951010d56a17436bb
NCrashed <NCrashed@gmail.com>
2014-05-24 12:49:57 +0400

    License change for shared files.

--- source/util/concomms/get.d
+++ source/util/concomms/get.d
@@ -1,12 +1,29 @@
-﻿//          Copyright Gushcha Anton, Shamyan Roman 2012.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          http://www.boost.org/LICENSE_1_0.txt)
+﻿// written in the D programming language
+/*
+*   This file is part of FostomEngine.
+*   
+*   FostomEngine is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*   
+*   FostomEngine is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*   
+*   You should have received a copy of the GNU General Public License
+*   along with FostomEngine.  If not, see <http://www.gnu.org/licenses/>.
+*/
 /**
-* @file get.d
-* Команда Get предназначена для вывода имформации о системе.
-* Например: $ get list выдает список зарегистрированных комманд
-* в мэнеджере комманд
+*   Copyright: © 2013-2014 Anton Gushcha, Shamyan Roman
+*   License: Subject to the terms of the GPL-3.0 license, as written in the included LICENSE file.
+*   Authors: Shamyan Roman,
+*            Anton Gushcha <ncrashed@gmail.com>         
+*
+*   Команда Get предназначена для вывода имформации о системе.
+*   Например: $ get list выдает список зарегистрированных комманд
+*   в мэнеджере комманд
 */
 
 module util.concomms.get;
