52621a2bf743057369d7106755ab064e9d6f3a78
NCrashed <NCrashed@gmail.com>
2014-06-03 23:09:09 +0400

    boxIntersect function documentation

--- source/client/shaders/raytrace/common.d
+++ source/client/shaders/raytrace/common.d
@@ -39,9 +39,9 @@ alias CommonKernels = Kernel!(MatrixKernels, "Common", q{
     *   rayDir          [out] direction of resulted ray in world space
     *   rayOrigin       [out] position of resulted ray in world space
     *   
-    *   Returns: -1 if cannot calculate ray, 0 if successful 
+    *   Returns: false if cannot calculate ray, true if successful 
     */
-    int getPixelRay(Matrix4x4 matProjViewInv, uint screenWidth, uint screenHeight, int idx, int idy, float3* rayDir, float3* rayOrigin)
+    bool getPixelRay(Matrix4x4 matProjViewInv, uint screenWidth, uint screenHeight, int idx, int idy, float3* rayDir, float3* rayOrigin)
     {
             float4 screenPos; 
             
@@ -53,7 +53,7 @@ alias CommonKernels = Kernel!(MatrixKernels, "Common", q{
             float4 vec1 = multiply(matProjViewInv, screenPos);
             if(vec1.w == 0)
             {
-                return -1;
+                return false;
             }
             vec1.w = 1.0f/vec1.w;
             vec1.x = vec1.x*vec1.w;
@@ -64,7 +64,7 @@ alias CommonKernels = Kernel!(MatrixKernels, "Common", q{
             float4 vec2 = multiply(matProjViewInv, screenPos);
             if(vec2.w == 0)
             {
-                return -1;
+                return false;
             }
             vec2.w = 1.0f/vec2.w;
             vec2.x = vec2.x*vec2.w;
@@ -78,6 +78,77 @@ alias CommonKernels = Kernel!(MatrixKernels, "Common", q{
             vec2.z = vec2.z - vec1.z;
             (*rayDir) = normalize(vec2.xyz);
             
-            return 0;
+            return true;
+    }
+    
+    /**
+    *   Calculates intersection between ray and axis aligned bounding box
+    *   Params:
+    *   rayDir      ray direction vector
+    *   rayOrigin   ray origin vector
+    *   minBox      box corner closest to the global origin
+    *   maxBox      box corner most farthest from the global origin
+    *   t0          [out] ray based coordinate of entering intersection
+    *   t1          [out] ray based coordinate of leaving intersection
+    *
+    *   Returns: true if there is intersection, false else.
+    */
+    bool boxIntersect(float3 rayDir, float3 rayOrigin, float3 minBox, float3 maxBox, float* t0, float* t1)
+    {
+        float tmin, tmax, tymin, tymax, tzmin, tzmax, div;
+        div = 1 / rayDir.x;
+        if (div >= 0)
+        {
+            tmin = (minBox.x - rayOrigin.x) * div;
+            tmax = (maxBox.x - rayOrigin.x) * div;
+        } else
+        {
+            tmin = (maxBox.x - rayOrigin.x) * div;
+            tmax = (minBox.x - rayOrigin.x) * div;
+        }
+        
+        div = 1 / rayDir.y;
+        if (div >= 0)
+        {
+            tymin = (minBox.y - rayOrigin.y) * div;
+            tymax = (maxBox.y - rayOrigin.y) * div;
+        } else
+        {
+            tymin = (maxBox.y - rayOrigin.y) * div;
+            tymax = (minBox.y - rayOrigin.y) * div;
+        }
+        
+        if ( (tmin > tymax) || (tymin > tmax) )
+        {
+            return false;
+        }
+        if (tymin > tmin)
+            tmin = tymin;
+        if (tymax < tmax)
+            tmax = tymax;
+            
+        div = 1 / rayDir.z; 
+        if (div >= 0)
+        {
+            tzmin = (minBox.z - rayOrigin.z) * div;
+            tzmax = (maxBox.z - rayOrigin.z) * div;
+        } else
+        {
+            tzmin = (maxBox.z - rayOrigin.z) * div;
+            tzmax = (minBox.z - rayOrigin.z) * div;
+        }
+        
+        if ( (tmin > tzmax) || (tzmin > tmax) )
+        {
+            return false;
+        }   
+        if (tzmin > tmin)
+            tmin = tzmin;
+        if (tzmax < tmax)
+            tmax = tzmax;
+        
+        *t0 = tmin;
+        *t1 = tmax;
+        return true;    
     }
 });
